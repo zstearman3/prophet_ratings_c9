e6420419408d05126090e91f6fdd97bd79bc61be
Zach Stearman <zstearman3@gmail.com>
2021-01-21 19:33:00 +0000

    Add initial header styling and layout

--- app/javascript/components/common/logged_in_header.jsx
+++ app/javascript/components/common/logged_in_header.jsx
@@ -1,15 +1,24 @@
 import React from "react"
-import ProphetLogo from 'images/prophet_logo.png'
+import ProphetLogo from 'images/prophet_logo_white.png'
 
 class Header extends React.Component {
   render () {
     return (
-      <div className="header">
-        <img src={ProphetLogo} />
-        <h1>
-          Welcome
-        </h1>
-      </div>
+      <nav className="navbar navbar-expand-lg">
+        <a className="navbar-brand" href="/">
+          <img src={ProphetLogo} width="200"/>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="/teams">Teams</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'static_pages#home'
+
+  devise_for :users
+  resources :teams, only: [:index]
 end
