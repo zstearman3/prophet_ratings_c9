4eedfbb33eb068cf27922ebaabcb115bec2f4200
Zach Stearman <zstearman3@gmail.com>
2021-01-21 20:22:14 +0000

    Break out nav link component

--- app/javascript/components/common/logged_in_header.jsx
+++ app/javascript/components/common/logged_in_header.jsx
@@ -1,6 +1,14 @@
 import React from "react"
 import ProphetLogo from 'images/prophet_logo_white.png'
 
+function NavLink(props) {
+  return(
+    <li className="nav-item">
+      <a className="nav-link" href={props.url}>{props.title}</a>
+    </li>
+  )
+}
+
 class Header extends React.Component {
   render () {
     return (
@@ -8,14 +16,12 @@ class Header extends React.Component {
         <a className="navbar-brand" href="/">
           <img src={ProphetLogo} width="200"/>
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="/teams">Teams</a>
-            </li>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <NavLink title="Teams" url="/teams" />
           </ul>
         </div>
       </nav>
